5f09710d17a438f8354b14041b0c8f1b41b773d3
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-03-23 13:49:19 +0300

    ScheduleSidebar - ScheduleHeader - BasicSidebarLabel

--- app/src/main/java/com/metehanbolat/freezerowandcolumncompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/freezerowandcolumncompose/MainActivity.kt
@@ -4,21 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.metehanbolat.freezerowandcolumncompose.ui.theme.FreezeRowAndColumnComposeTheme
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -88,4 +95,83 @@ fun BasicEvent(
             )
         }
     }
+}
+
+@Composable
+fun BasicDayHeader(
+    day: LocalDate,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = day.format(DayFormatter),
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(4.dp)
+    )
+}
+
+@Composable
+fun ScheduleHeader(
+    minDate: LocalDate,
+    maxDate: LocalDate,
+    dayWidth: Dp,
+    modifier: Modifier = Modifier,
+    dayHeader: @Composable (day: LocalDate) -> Unit = { BasicDayHeader(day = it) }
+) {
+    Row(
+        modifier = modifier
+    ) {
+        val numDays = ChronoUnit.DAYS.between(minDate, maxDate).toInt() + 1
+        repeat(numDays) { i ->
+            Box(
+                modifier = Modifier
+                    .width(dayWidth)
+                    .height(40.dp)
+                    .background(Color.Green)
+            ) {
+                dayHeader(minDate.plusDays(i.toLong()))
+            }
+        }
+    }
+}
+
+@Composable
+fun ScheduleSidebar(
+    hourHeight: Dp,
+    modifier: Modifier = Modifier,
+    label: @Composable (time: LocalTime) -> Unit = { BasicSidebarLabel(time = it) }
+) {
+    Column(
+        modifier = modifier
+    ) {
+        val startTime = LocalTime.MIN
+        repeat(24) { i ->
+            Box(
+                modifier = Modifier
+                    .height(hourHeight)
+                    .width(60.dp)
+                    .background(Color.Red)
+            ) {
+                label(startTime.plusHours(i.toLong()))
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicSidebarLabel(
+    time: LocalTime,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = time.format(HourFormatter),
+        modifier = modifier
+            .fillMaxHeight()
+            .padding(4.dp),
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
 }
